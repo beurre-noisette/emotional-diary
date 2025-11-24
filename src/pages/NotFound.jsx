a07326efd03893dd61f32ee2588fb06fd2d4e1aa
Sangwon Seo <tjtkddnjs857@naver.com>
2025-11-24 13:05:02 +0900

    feat: initial commit

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;

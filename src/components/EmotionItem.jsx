666894ec9da3a4d8fec36d0cf129ee4e525cdff4
Sangwon Seo <tjtkddnjs857@naver.com>
2025-11-26 13:38:13 +0900

    feat: New 컴포넌트 기능 구현

---
+++ src/components/EmotionItem.jsx
@@ -0,0 +1,18 @@
+import "./EmotionItem.css";
+import { getEmotionImage } from "../util/get-emotion-image.js";
+
+const EmotionItem = ({ emotionId, emotionName, isSelected, onClick }) => {
+  return (
+    <>
+      <div
+        className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}
+        onClick={onClick}
+      >
+        <img src={getEmotionImage(emotionId)} className="emotion_img" />
+        <div className="emotion_name">{emotionName}</div>
+      </div>
+    </>
+  );
+};
+
+export default EmotionItem;
